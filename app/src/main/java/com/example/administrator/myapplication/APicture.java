b65a183bb60a54eeefadfcb1da91090850541737
CoralZhong <1049170629@qq.com>
2016-05-27 17:08:44 +0800

    initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/APicture.java
@@ -0,0 +1,17 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/5/26.
+ */
+public class APicture extends BaseActivity {
+
+
+
+    @Override
+    protected void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.apicture);
+    }
+}
