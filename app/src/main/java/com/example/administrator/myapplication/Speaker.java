53ab15d141211cb16f7350e80eb45db4bd78ff45
CoralZhong <1049170629@qq.com>
2016-06-06 09:50:52 +0800

    initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/Speaker.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/6/6.
+ */
+public class Speaker extends BaseActivity {
+    @Override
+    void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.spraker);
+
+
+    }
+}
